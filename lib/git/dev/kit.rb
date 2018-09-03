cb08b113a6346d10514647079b51371969396857
Matthew Eagar <meagar@gmail.com>
2018-09-03 13:54:46 -0400

    Iniital "bundle gem"

---
+++ lib/git/dev/kit.rb
@@ -0,0 +1,9 @@
+require "git/dev/kit/version"
+
+module Git
+  module Dev
+    module Kit
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/git/dev/kit/version.rb
@@ -0,0 +1,7 @@
+module Git
+  module Dev
+    module Kit
+      VERSION = "0.1.0"
+    end
+  end
+end
